d252b3f63a35c66490e7817f68ca22a40d4a7941
Sebi <Polarfuchs123@gmx.de>
2020-04-28 12:21:55 +0200

    Default bind for team voice changed
    Change the default key for the team voicechat

--- gamemodes/terrortown/gamemode/client/cl_voice.lua
+++ gamemodes/terrortown/gamemode/client/cl_voice.lua
@@ -90,7 +90,7 @@ end
 bind.Register("ttt2_voice", VoiceTryEnable, VoiceTryDisable, "TTT2 Bindings", "f1_bind_voice", input.GetKeyCode(input.LookupBinding("+voicerecord") or KEY_X))
 
 -- register a binding for the team voicechat
-bind.Register("ttt2_voice_team", VoiceTeamTryEnable, VoiceTeamTryDisable, "TTT2 Bindings", "f1_bind_voice_team", input.GetKeyCode(input.LookupBinding("+speed") or KEY_LSHIFT))
+bind.Register("ttt2_voice_team", VoiceTeamTryEnable, VoiceTeamTryDisable, "TTT2 Bindings", "f1_bind_voice_team", input.GetKeyCode(KEY_ASTERISK))
 
 -- 255 at 100
 -- 5 at 5000
